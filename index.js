1087549ef7365f714fad3b5addf3f5569956d8af
RakibHossen678 <rh491464@gmail.com>
2024-05-05 23:48:39 +0600

    retrieve single data from database

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -40,6 +40,13 @@ async function run() {
     app.get('/jobs',async(req,res)=>{
         const result=await jobsCollection.find().toArray()
         res.send(result)
+    })
+
+    app.get('/jobs/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id : new ObjectId(id)}
+      const result=await jobsCollection.findOne(query)
+      res.send(result)
     })
 
 
